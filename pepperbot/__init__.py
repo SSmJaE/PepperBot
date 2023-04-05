c15248d21a84f65d97d97a5340fa62ffdf1b2905
EdIfiMr <shaoxydd8888@gmail.com>
2023-04-05 22:21:03 +0800

    fix: better exception encoding to utf-8

--- pepperbot/__init__.py
+++ pepperbot/__init__.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
-__version__ = "0.3.0"
-
 import better_exceptions
+from better_exceptions import encoding
+
 from .initial import PepperBot
 
+__version__ = "0.3.1"
+__all__ = ("PepperBot", "__version__")
+
+# https://github.com/Qix-/better-exceptions/issues/53
+# 中文windows默认编码是gb2312，管道符号无法正常显示，手动设置为utf-8
+encoding.ENCODING = "utf-8"
 
 better_exceptions.hook()
